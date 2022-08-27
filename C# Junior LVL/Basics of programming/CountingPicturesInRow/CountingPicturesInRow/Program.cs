56d514e333e8d77791611acd4fd01113c94094fe
Maxim Sidorenko <maxim.sidorenk15@gmail.com>
2022-08-27 22:46:48 +0300

    Finshed Task
    Finshed Task: https://lk.ijunior.ru/Homework/Detail/28

---
+++ C# Junior LVL/Basics of programming/CountingPicturesInRow/CountingPicturesInRow/Program.cs
@@ -0,0 +1,21 @@
+﻿//Task of https://lk.ijunior.ru/Homework/Detail/28
+using System;
+
+namespace CountingPicturesInRow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var picturesRowLength = 3;
+
+            var userPicturesCount = 52;
+
+            var filledPicturesRowsCount = userPicturesCount / picturesRowLength;
+            var overRowPicturesCount = userPicturesCount % picturesRowLength;
+
+            Console.WriteLine($"Количество картинок игрока - {userPicturesCount}, картинки смогли полностью заполнить {filledPicturesRowsCount}, с остатков в размере {overRowPicturesCount}.");
+            Console.ReadKey();
+        }
+    }
+}

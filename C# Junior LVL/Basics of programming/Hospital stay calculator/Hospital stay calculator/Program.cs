dd37bdbead04b8640e33daf2a2b8031a95189a11
Maxim Sidorenko <maxim.sidorenk15@gmail.com>
2022-08-28 01:44:14 +0300

    Finished Tasks
    Finished Tasks: https://lk.ijunior.ru/Homework/Detail/30

---
+++ C# Junior LVL/Basics of programming/Hospital stay calculator/Hospital stay calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalStayCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var doctorAppointmentTimeInMinutes = 10;
+            var minutesPerHour = 60;
+
+            Console.Write($"Перед вами стоит: ");
+            var visitorsBeforeUser = Convert.ToInt32(Console.ReadLine());
+
+            var waitingTimeInMinutes = visitorsBeforeUser * doctorAppointmentTimeInMinutes;
+
+            var roundedWaitingTimeInMinutes = waitingTimeInMinutes % minutesPerHour;
+            var waitingTimeInHours = waitingTimeInMinutes / minutesPerHour;
+
+            Console.WriteLine($"Вы будете ждать в очереди {waitingTimeInHours}:{roundedWaitingTimeInMinutes}.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C# Junior LVL/Basics of programming/Hospital stay calculator/Hospital stay calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Hospital stay calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Hospital stay calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("080c723c-0bb4-4195-b350-66e5b23c83bc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

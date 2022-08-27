3d254c175d72e3970f12b828c8732f5956761a2e
Maxim Sidorenko <maxim.sidorenk15@gmail.com>
2022-08-27 23:05:50 +0300

    Finished Tasks
    Fineshed Tasks:
    https://lk.ijunior.ru/Homework/Detail/201

---
+++ C# Junior LVL/Basics of programming/SwapVariables/SwapVariables/Program.cs
@@ -0,0 +1,18 @@
+﻿//Task of https://lk.ijunior.ru/Homework/Detail/201
+
+namespace SwapVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var userFamily = "Olexay";
+            var userName = "Sahorow";
+
+            //Split user name and family.
+            var temp = userName;
+            userName = userFamily;
+            userFamily = temp;
+        }
+    }
+}

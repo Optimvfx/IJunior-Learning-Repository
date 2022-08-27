6b6b94253f98ec9f889649f2ae52755f3816cce0
Maxim Sidorenko <maxim.sidorenk15@gmail.com>
2022-08-27 19:29:21 +0300

    Redacted Tasks
    Redacted Tasks:
    https://lk.ijunior.ru/Homework/Detail/56

--- C# Junior LVL/Basics of programming/InputToUserInfo/InputToUserInfo/Program.cs
+++ C# Junior LVL/Basics of programming/InputToUserInfo/InputToUserInfo/Program.cs
@@ -7,13 +7,17 @@ namespace InputToUserInfo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите свое имя: ");
             var userName = Console.ReadLine();
-           
+
+            Console.WriteLine("Введите свою должность: ");
             var userPost = Console.ReadLine();
 
-            var userSalary = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите свою зарплату: ");
+            var userSalaryInUSD = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"Вас зовут {userName},Вы работаете на посту {userPost} и получаете зарплату в размере {userSalary}.");
+            Console.WriteLine($"Вас зовут {userName},Вы работаете на посту {userPost} и получаете зарплату в размере {userSalaryInUSD} USD.");
+            Console.ReadKey();
         }
     }
 }

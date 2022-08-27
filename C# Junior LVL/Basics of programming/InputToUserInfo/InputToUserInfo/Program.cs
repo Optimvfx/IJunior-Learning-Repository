42657a14963cc53f3e0820b6fe12d6cc2ca6030b
Maxim Sidorenko <maxim.sidorenk15@gmail.com>
2022-08-27 19:13:27 +0300

    Finshed Tasks
    Finshed Tasks:
    https://lk.ijunior.ru/Homework/Detail/58

---
+++ C# Junior LVL/Basics of programming/InputToUserInfo/InputToUserInfo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//Task of https://lk.ijunior.ru/Homework/Detail/58
+namespace InputToUserInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var userName = Console.ReadLine();
+           
+            var userPost = Console.ReadLine();
+
+            var userSalary = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Вас зовут {userName},Вы работаете на посту {userPost} и получаете зарплату в размере {userSalary}.");
+        }
+    }
+}

1064b8719b34c363f006f57f8f05cb1cd3eb902e
Maxim Sidorenko <maxim.sidorenk15@gmail.com>
2022-08-28 00:34:56 +0300

    Finished Tasks
    Finished Tasks:
    https://lk.ijunior.ru/Homework/Detail/29

---
+++ C# Junior LVL/Basics of programming/Kristal Shop/Kristal Shop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Kristal_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int crystalPrice = 5;
+
+            int crystalCount = 0;
+            Console.Write("У вас монет: ");
+            int goldCount = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Сколько вы хотите купить крисаталов по цене {crystalPrice}?");
+            int buyedCristalCount = Convert.ToInt32(Console.ReadLine());
+             
+            goldCount -= buyedCristalCount * crystalPrice;
+            crystalCount += buyedCristalCount;
+
+            Console.WriteLine($"У вас: {goldCount} золота, {crystalCount} кристалов.");
+            Console.ReadKey();
+        }
+    }
+}

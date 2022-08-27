67d20455f6d87256651eae788daf644393416bdf
Maxim Sidorenko <maxim.sidorenk15@gmail.com>
2022-08-27 16:47:30 +0300

    Redacte Tasks
    Redacted Tasks:
    https://lk.ijunior.ru/Homework/Detail/26

--- C# Junior LVL/Basics of programming/Create 10 Variables Task/Create 10 Variables Task/Program.cs
+++ C# Junior LVL/Basics of programming/Create 10 Variables Task/Create 10 Variables Task/Program.cs
@@ -8,43 +8,22 @@ namespace Create_10_Variables_Task
     {
         static void Main(string[] args)
         {
-            var classWithTenFields = new ClassWithTenFields();
+            uint MaximalHealth = 200;
+            int CurrentHealth = (int)MaximalHealth;
 
-            classWithTenFields.MaximalHealth = 200;
-            classWithTenFields.CurrentHealth = (int)classWithTenFields.MaximalHealth;
+            byte CharByteCode = (byte)'h';
 
-            classWithTenFields.CharByteCode = (byte)'h';
+            sbyte ColorPaletteRedForce = 42;
 
-            classWithTenFields.ColorPaletteRedForce = 42;
+            long UserCardNumber = 4127631616283717;
+            string UserName = "Stepan Valeriev";
 
-            classWithTenFields.UserCardNumber = 4127631616283717;
-            classWithTenFields.UserName = "Stepan Valeriev";
+            float PlayerMoveSpeed = 15f;
+            double GrawityForce = 9.8;
 
-            classWithTenFields.PlayerMoveSpeed = 15f;
-            classWithTenFields.GrawityForce = 9.8;
-
-            classWithTenFields.ExceptedRegistrationConfirmationCode = 1526;
+            short ExceptedRegistrationConfirmationCode = 1526;
             var registrationConfirmationCode = 1526;
-            classWithTenFields.RegistrationCompletedSuccessfully = registrationConfirmationCode == classWithTenFields.ExceptedRegistrationConfirmationCode;
-        }
-
-        protected class ClassWithTenFields
-        {
-            public uint MaximalHealth { get; set; }
-            public int CurrentHealth { get; set; }
-
-            public byte CharByteCode { get; set; }
-
-            public sbyte ColorPaletteRedForce { get; set; }
-
-            public long UserCardNumber { get; set; }
-            public string UserName { get; set; }
-
-            public float PlayerMoveSpeed { get; set; }
-            public double GrawityForce { get; set; }
-
-            public short ExceptedRegistrationConfirmationCode { get; set; }
-            public bool RegistrationCompletedSuccessfully { get; set; }
+            bool RegistrationCompletedSuccessfully = registrationConfirmationCode == ExceptedRegistrationConfirmationCode;
         }
     }   
 }

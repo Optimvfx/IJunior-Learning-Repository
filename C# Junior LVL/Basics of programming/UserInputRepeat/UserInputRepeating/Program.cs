f94169804b70d07ae127cc805dd39771848e09b4
Maxim Sidorenko <maxim.sidorenk15@gmail.com>
2022-08-29 09:20:37 +0300

    Finished Tasks
    Finished Tasks:
    https://lk.ijunior.ru/Homework/Detail/121

---
+++ C# Junior LVL/Basics of programming/UserInputRepeat/UserInputRepeating/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UserInputRepeating
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint minimalRepeatCount = uint.MinValue;
+
+            Console.Write("Введите строку: ");
+            var userInputString = Console.ReadLine();
+            Console.Write("Введите количество повторов: ");
+            var repeatCount = Convert.ToInt32(Console.ReadLine());
+            
+            if(repeatCount < minimalRepeatCount)
+            {
+                Console.WriteLine($"Вы ввели {repeatCount}, что меньше чем минимально допустимое значение({minimalRepeatCount})");
+                Console.ReadKey();
+                return;
+            }
+
+            for(int repeat = 1; repeat <= repeatCount; repeat++)
+            {
+                Console.WriteLine($"{userInputString} || Повтор номер {repeat}");
+                Console.ReadKey();
+            }
+        }
+    }
+}

8bc1958160a705cff7c41c1232ec4997614d0308
Maxim Sidorenko <maxim.sidorenk15@gmail.com>
2022-08-31 20:07:31 +0300

    Finished Tasks

---
+++ C# Junior LVL/Arrays/FindTheLargestElementInArray/FindTheLargestElementInArray/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace FindTheLargestElementInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new float[10, 10];
+
+            #region randomFillArray
+            var random = new Random();
+
+            var minimalRandomNumber = 0;
+            var maximalRandomNumber = 100;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(minimalRandomNumber, maximalRandomNumber);
+                }
+            }
+            #endregion randomFillArray
+
+            var maximalElement = float.MinValue;
+
+            foreach(var element in array)
+            {
+                if (element >= maximalElement)
+                    maximalElement = element;
+            }
+
+            var newValueOfMaximalElements = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"\nMaximal element is {maximalElement}.\n");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for(int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] >= maximalElement)
+                        array[i, j] = newValueOfMaximalElements;
+
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

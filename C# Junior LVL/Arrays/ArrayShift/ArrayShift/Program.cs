fca3ac4725f7a62d5274f52da347c3d96e653acb
Maxim Sidorenko <maxim.sidorenk15@gmail.com>
2022-09-01 06:41:01 +0300

    Finished Tasks

---
+++ C# Junior LVL/Arrays/ArrayShift/ArrayShift/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ArrayShift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new float[] {1,2,3,4};
+
+            foreach (var value in array)
+                Console.WriteLine(value + " ");
+
+            Console.Write("Enter array shift: ");
+            int arrayShift = Convert.ToInt32(Console.ReadLine());
+
+            for (int shift = 0; shift < arrayShift; shift++)
+            {
+                var prewiusNumber = array[array.Length - 1];
+                array[array.Length - 1] = array[0];
+
+                for (int i = array.Length - 2; i >= 0; i--)
+                {
+                    var temp = prewiusNumber;
+                    prewiusNumber = array[i];
+                    array[i] = temp;
+                }
+            }
+            foreach (var value in array)
+                Console.WriteLine(value);
+
+            Console.ReadKey();
+        }
+    }
+}

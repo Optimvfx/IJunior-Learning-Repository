8e6644d77ce746a7d5cf05d7a3dde0be9d5e890a
Maxim Sidorenko <maxim.sidorenk15@gmail.com>
2022-09-02 23:56:32 +0300

    Redacted Tasks

---
+++ C# Junior LVL/Functions/InputToIntConvertor/InputToIntConvertor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InputToIntConvertor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetUserInputInt();
+
+            Console.ReadKey();
+        }
+
+        public static int GetUserInputInt()
+        {
+            int userInput;
+            Console.Write("Enter a number:");
+            
+            while(int.TryParse(Console.ReadLine(), out userInput) == false)
+            {
+                Console.Write("Invalid Input, enter a number:");
+            }
+
+            return userInput;
+        }
+    }
+}

03946459db25e9c166e7c1c1fa2dab534891dddb
Maxim Sidorenko <maxim.sidorenk15@gmail.com>
2022-09-02 22:49:43 +0300

    Finished Tasks

---
+++ C# Junior LVL/Functions/DrowBar/DrowBar/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DrowBar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawBar(56f, 20, ConsoleColor.Red);
+            DrawBar(80f, 30, ConsoleColor.Blue, PositionY: 1);
+
+            Console.ReadKey();
+        }
+
+        public static void DrawBar(float fillProcent,uint barLength, ConsoleColor fillColor, ConsoleColor unFillColor = ConsoleColor.Black,int positionX = 0,int PositionY = 0, char fillChar = '#',char unFillChar = '_')
+        {
+            float minimalFillProcent = 0f;
+            float maximalFillProcent = 100f;
+
+            fillProcent = Math.Max(Math.Min(fillProcent, maximalFillProcent), minimalFillProcent);
+            int relativeProcent = (int)(fillProcent / 100 * barLength);
+
+            Console.SetCursorPosition(positionX, PositionY);
+
+            var prewiusColor = Console.BackgroundColor;
+            Console.Write("[");
+            
+            for(int i = 0; i < barLength; i++)
+            {
+                if(relativeProcent > i)
+                {
+                    Console.BackgroundColor = fillColor;
+                    Console.Write(fillChar);
+                }
+                else
+                {
+                    Console.BackgroundColor = unFillColor;
+                    Console.Write(unFillChar);
+                }
+            }
+
+            Console.BackgroundColor = prewiusColor;
+            Console.Write("]");
+        }
+    }
+}

42c8c71c15197f6e1a436b613bc65e88640c671b
Maxim Sidorenko <maxim.sidorenk15@gmail.com>
2022-09-03 04:24:06 +0300

    Finished Tasks

---
+++ C# Junior LVL/Functions/ShuffleArray/ShuffleArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ShuffleArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[]{1,2,3,4,5,6,7,8,9,10};
+
+            Shuffle(array,new Random());
+
+            foreach (var element in array)
+                Console.WriteLine(element);
+
+            Console.ReadKey();
+        }
+
+        private static int[] Shuffle(int[] array, Random random)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int randomIndex = random.Next(array.Length);
+                int temp = array[i];
+                array[i] = array[randomIndex];
+                array[randomIndex] = temp;
+            }
+
+            return array;
+        }
+    }
+}

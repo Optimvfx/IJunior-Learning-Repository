2e1fbce85d5b11e03a2a491aef18d6177b3c3f25
Maxim Sidorenko <maxim.sidorenk15@gmail.com>
2022-09-04 05:15:10 +0300

    Finished Tasks

---
+++ C# Junior LVL/Collections/ConsoleCalculatorList/ConsoleCalculatorList/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleCalculatorList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ExitCommand = "EXIT";
+            const string SumCommand = "SUM";
+
+            var numbers = new List<int>();
+
+            var isOpen = true;
+
+            while (isOpen)
+            {
+                Console.WriteLine($"\nEneter command, commands:" +
+                    $"\nenter number(write a number)." +
+                    $"\n{SumCommand} - summar all numbers." +
+                    $"\n{ExitCommand} - exit a program.");
+
+                string userInput = Console.ReadLine().ToUpper();
+
+                switch (userInput)
+                {
+                    case SumCommand:
+                        var sumOfAllNumbers = numbers.Sum();
+                        Console.WriteLine($"Sum of all user input is {sumOfAllNumbers}.");
+                        break;
+                    default:
+                        var userInputNumber = GetUserInputInt(userInput);
+                        numbers.Add(userInputNumber);
+                        break;
+                    case ExitCommand:
+                        isOpen = false;
+                        break;
+                }
+            }
+        }
+
+        private static int GetUserInputInt(string userInput = "")
+        {
+            var userInputInt = 0;
+
+            while (int.TryParse(userInput, out userInputInt) == false)
+            {
+                Console.Write("Invalid Input, enter a number:");
+                userInput = Console.ReadLine();
+            }
+
+            return userInputInt;
+        }
+    }
+}

d2728c6bbd998b6e9ed0f33c5c01cce24ae45c48
Maxim Sidorenko <maxim.sidorenk15@gmail.com>
2022-09-09 13:27:57 +0300

    Finished Tasks

---
+++ C# Junior LVL/OOP/PlayerClass/PlayerClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PlayerClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var player = new Player("Joo", 100, 50);
+        }
+
+        private class Player
+        {
+            public string Name { get; private set; }
+
+            public int MaximalHealth { get; private set; }
+            public int CurrentHealth { get; private set; }
+
+            public int MaximalEnergy { get; private set; }
+            public int CurrentEnergy { get; private set; }
+
+            public Player(string name, int maximalHealth, int maximalEnergy)
+            {
+                Name = name;
+
+                MaximalHealth = maximalHealth;
+                CurrentHealth = maximalHealth;
+
+                MaximalEnergy = maximalEnergy;
+                CurrentEnergy = maximalEnergy;
+            }
+        }
+    }
+}
